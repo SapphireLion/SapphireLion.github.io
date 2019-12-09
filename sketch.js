e038dfa4344ba8182554105adf97f4aceffb2a73
SapphireLion <kevin.torres@ttu.edu>
2019-12-08 18:35:57 -0600

    fff

--- sketch.js
+++ sketch.js
@@ -184,7 +184,6 @@ function setup() {
     questionDiv = createDiv();
     rulesDiv = createDiv();
     feedbackDiv = createDiv('You have not answered yet!');
-<<<<<<< HEAD
     feedbackDiv.position(170,670);
     feedbackDiv.style('font-size', '200%');
 
@@ -199,14 +198,6 @@ function setup() {
     quittext.style('position','absolute');
     quittext.style('top','35%');
     quittext.style('left','5%');
-=======
-    feedbackDiv.position(650,650);
-    feedbackDiv.style('font-size', '200%');
-
-    winorlose = createDiv();
-    winorlose.position(700,700);
-    winorlose.style('font-weight', 'bold');
->>>>>>> 76b2f878f2a4e3b2dcf737a1aa5105e3f9f344fd
 
     rulesDiv.position(800,0);
     rulesDiv.size(450, AUTO);
@@ -560,7 +551,6 @@ function checkE(){
         feedbackDiv.html("You are incorrect! Sorry!");
         feedbackDiv.style("color", "red");
     }
-<<<<<<< HEAD
 
 }
 
@@ -618,6 +608,4 @@ function quit()
   feedbackDiv.style("color", "black");
   minutes = 9;
   seconds = 0;
-=======
->>>>>>> 76b2f878f2a4e3b2dcf737a1aa5105e3f9f344fd
 }
